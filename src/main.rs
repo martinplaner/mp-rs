5c5478f774b1067dd1df016ea1f946fd9f55cd44
Martin Planer <martin.planer@gmail.com>
2024-11-01 12:00:00 +0100

    Option return type for generator

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,19 @@ fn main() {
     println!("{:#?}", generate_word("mp", &dict));
     println!("{:#?}", generate_word("mp", &dict));
     println!("{:#?}", generate_word("mp", &dict));
+    println!("{:#?}", generate_word("abc", &dict));
 }
 
-fn generate_word(term: &str, dict: &HashMap<char, Vec<String>>) -> String {
+fn generate_word(term: &str, dict: &HashMap<char, Vec<String>>) -> Option<String> {
+    let mut rng = thread_rng();
+
     term.chars()
         .map(|c| {
-            let words = dict.get(&c).unwrap();
-            let i = thread_rng().gen_range(0..words.len());
-            words.get(i).unwrap().to_owned()
+            dict.get(&c).and_then(|words| {
+                let i = rng.gen_range(0..words.len());
+                words.get(i).map(|s| s.as_str())
+            })
         })
-        .collect::<Vec<String>>()
-        .join("-")
+        .collect::<Option<Vec<&str>>>()
+        .map(|words| words.join("-"))
 }
