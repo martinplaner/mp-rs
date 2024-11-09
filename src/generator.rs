fd49cd687653225ce85bbf9049ad32423b5533b6
Martin Planer <martin.planer@gmail.com>
2024-11-09 10:40:07 +0100

    Add tests

--- src/generator.rs
+++ src/generator.rs
@@ -15,7 +15,7 @@ impl CompoundGenerator {
     pub fn new(reader: impl BufRead, delimiter: &str) -> Result<CompoundGenerator, Error> {
         let mut dict: HashMap<char, Vec<String>> = HashMap::new();
         for line in reader.lines() {
-            let line = line?;
+            let line = line?.trim().to_owned();
 
             if line.starts_with('#') {
                 continue;
@@ -38,15 +38,44 @@ impl Generator for CompoundGenerator {
     fn generate(&self, term: &str) -> Option<String> {
         let mut rng = thread_rng();
 
-        term.to_uppercase()
+        let words = term
+            .to_uppercase()
             .chars()
-            .map(|c| {
+            .filter_map(|c| {
                 self.dict.get(&c).and_then(|words| {
                     let i = rng.gen_range(0..words.len());
                     words.get(i).map(|s| s.to_owned())
                 })
             })
-            .collect::<Option<Vec<String>>>()
-            .map(|words| words.join(&self.delimiter))
+            .collect::<Vec<String>>();
+
+        if words.is_empty() {
+            None
+        } else {
+            Some(words.join(&self.delimiter))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::generator::{CompoundGenerator, Generator};
+    use std::io::BufReader;
+
+    #[test]
+    fn generate_some() {
+        let dict = "#comment\nAlpha\n# comment\n   Bravo".as_bytes();
+        let generator = CompoundGenerator::new(dict, "-").unwrap();
+
+        assert_eq!(generator.generate("ab"), Some("Alpha-Bravo".to_string()));
+    }
+
+    #[test]
+    fn generate_none() {
+        let reader = BufReader::new(std::io::empty());
+        let generator = CompoundGenerator::new(reader, "-").unwrap();
+
+        assert_eq!(generator.generate("ab"), None);
+        assert_eq!(generator.generate(""), None);
     }
 }
